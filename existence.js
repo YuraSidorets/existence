c9bb6ceaff14dd902bb01ac2ce6ae76d1ffa84e4
Yurii Sydorets <sidoretsyura@gmail.com>
2019-07-24 22:12:12 +0300

    circles added

--- existence.js
+++ existence.js
@@ -1,12 +1,27 @@
+let circles = [];
+let months = 1080;
+
 function setup() {
     createCanvas(1280, 760);
+    for (let i = 0; i < months; i++) {
+        circles.push(new Circle());
+    }
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].display();
     }
-    ellipse(mouseX, mouseY, 80, 80);
+}
+
+class Circle {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+        this.diameter = 30;
+    }
+
+    display() {
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
 }
